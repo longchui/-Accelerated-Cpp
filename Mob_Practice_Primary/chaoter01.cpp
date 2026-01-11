f83508092c78c69b1fdf14647ce30ffedc85aa94
mob <2451560887@qq.com>
2026-01-11 21:04:44 +0800

    前四章，快速略过

---
+++ Mob_Practice_Primary/chaoter01.cpp
@@ -0,0 +1,17 @@
+//
+// Created by mob on 2026/1/11.
+//
+
+#include "chaoter01.h"
+
+#include <iostream>
+#include "string"
+int main(){
+    using std::string;
+    std::string s;
+    std::cout << "Please enter your first name: ";
+    for (auto i = 0; i < 6; ++i) {
+        continue;
+    }
+
+}
